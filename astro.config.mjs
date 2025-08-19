f4b6295aa80df3d30a4cca896e658258cec7e56b
bmancilla23 <bmancilla119@gmail.com>
2025-08-18 19:58:46 -0500

    feat: add MainLayout component and integrate Tailwind CSS
    - Updated package.json to include Tailwind CSS and Vite integration.
    - Created MainLayout.astro for consistent layout structure across pages.
    - Refactored index.astro to use MainLayout and added a sample heading.
    - Enhanced tsconfig.json with path aliases for easier imports.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+
+import path from "node:path";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        "@": path.resolve("./src"),
+      },
+    },
+    plugins: [tailwindcss()],
+  },
+});
