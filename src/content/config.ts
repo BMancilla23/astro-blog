7f9e6f7268e8c14c5dc4f2b431d6c3dd58d73627
bmancilla23 <bmancilla119@gmail.com>
2025-08-23 21:36:17 -0500

    refactor(config): improve image validation logic in blog schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,20 +1,35 @@
 import { defineCollection } from "astro/content/config";
 import { z } from "astro:content";
 
+
 const blogCollection = defineCollection({
   type: "content",
-  schema: z.object({
-    title: z.string().min(1),
-    date: z.date(),
-    description: z.string().min(1),
-    image: z.string().optional(),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string().min(1),
+      date: z.date(),
+      description: z.string().min(1),
+      /* image: z.string().optional(), */
+      image: image()
+        .superRefine((img, ctx) => {
+          if (img && (img.width < 400 || img.width > 4000)) {
+            ctx.addIssue({
+              code: z.ZodIssueCode.custom,
+              message: "La imagen debe estar entre 400px y 4000px de ancho",
+            });
+          }
+        })
+        /*  .refine((img) => img.width >= 100, {
+          message: "La imagen debe estar entre 400px y 4000px de ancho",
+        }) */
+        .optional(),
 
-    // TODO: Relation
-    author: z.string().min(1).default("Bryan Mancilla"),
+      // TODO: Relation
+      author: z.string().min(1).default("Bryan Mancilla"),
 
-    // TODO: Relaction
-    tags: z.string().array().optional(),
-  }),
+      // TODO: Relaction
+      tags: z.string().array().optional(),
+    }),
 });
 
 export const collections = {
